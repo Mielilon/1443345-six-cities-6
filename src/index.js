832b8a3d730b0cb4119dbb97d0501e2ba0b8661d
Mielilon <daria.mg354@mail.ru>
2021-02-04 09:59:50 +0300

    Создание компонентов главной страницы приложения

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/app/app';
+// import Card from './components/card/card';
+// mport PropTypes from 'prop-types';
+
+
+ReactDOM.render(
+    <App/>,
+    document.getElementById(`root`)
+);
