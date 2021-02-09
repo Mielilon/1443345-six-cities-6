59c65e3d24340e95de3722a17e7f61a6e9d51f6a
Mielilon <daria.mg354@mail.ru>
2021-02-09 10:35:59 +0300

    Настройка маршрутизации

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,9 +1,38 @@
 import React from 'react';
-import MainScreen from '../main-screen/main-screen';
 import PropTypes from 'prop-types';
+import {Switch, Route, BrowserRouter} from 'react-router-dom';
+
+import MainScreen from '../main-screen/main-screen';
+import Favorites from '../favorites/favorites.jsx';
+import Login from '../login/login.jsx';
+import OfferPage from '../offer-page/offer-page.jsx';
+import NotFoundScreen from '../not-found-screen/not-found-screen.jsx';
+
 
 const App = ({offers}) => {
-  return <MainScreen offers={offers}/>;
+  return (
+    <React.Fragment>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/">
+            <MainScreen offers={offers}/>
+          </Route>
+          <Route exact path="/favorites">
+            <Favorites />
+          </Route>
+          <Route exact path="/login">
+            <Login />
+          </Route>
+          <Route exact path="/offer/:id">
+            <OfferPage />
+          </Route>
+          <Route>
+            <NotFoundScreen />
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    </React.Fragment>
+  );
 };
 
 App.propTypes = {
