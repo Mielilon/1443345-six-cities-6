a4da5ac7aeb559c92003bbb62babb5b9d00e121f
Mielilon <daria.mg354@mail.ru>
2021-03-02 19:19:42 +0300

    Настройка Redux, распределение карточек по городам, настройка фильтрации

--- src/components/list-of-offers/list-of-offers.jsx
+++ src/components/list-of-offers/list-of-offers.jsx
@@ -1,27 +1,15 @@
-import React from 'react';
+import React, {useState} from 'react';
 import PropTypes from 'prop-types';
+import PlacesSorting from '../offers-sorting/offers-sorting';
 import Offer from '../offer/offer';
 
-const ListOfOffers = ({offers}) => {
+const ListOfOffers = ({offers, location}) => {
+  const [sortingType, setSortingType] = useState(`Popular`);
   return (
     <section className="cities__places places">
       <h2 className="visually-hidden">Places</h2>
-      <b className="places__found">{offers.length} places to stay in Amsterdam</b>
-      <form className="places__sorting" action="#" method="get">
-        <span className="places__sorting-caption">Sort by</span>
-        <span className="places__sorting-type" tabIndex="0">
-                  Popular
-          <svg className="places__sorting-arrow" width="7" height="4">
-            <use xlinkHref="#icon-arrow-select" />
-          </svg>
-        </span>
-        <ul className="places__options places__options--custom places__options--opened">
-          <li className="places__option places__option--active" tabIndex="0">Popular</li>
-          <li className="places__option" tabIndex="0">Price: low to high</li>
-          <li className="places__option" tabIndex="0">Price: high to low</li>
-          <li className="places__option" tabIndex="0">Top rated first</li>
-        </ul>
-      </form>
+      <b className="places__found">{offers.length} places to stay in {location}</b>
+      <PlacesSorting activeOption={sortingType} changeOption={setSortingType} />
       <div className="cities__places-list places__list tabs__content">
         {offers.map((offer, i) => <Offer key={offer.title + i} {...offer}/>)}
       </div>
@@ -31,6 +19,7 @@ const ListOfOffers = ({offers}) => {
 
 ListOfOffers.propTypes = {
   offers: PropTypes.array.isRequired,
+  location: PropTypes.string.isRequired,
 };
 
 export default ListOfOffers;
