912d578bf72e350f3bb1edaeeb8502389ab61a0f
Mielilon <daria.mg354@mail.ru>
2021-03-03 16:05:59 +0300

    Подсветка маркера

--- src/components/list-of-offers-nearby/list-of-offers-nearby.jsx
+++ src/components/list-of-offers-nearby/list-of-offers-nearby.jsx
@@ -2,12 +2,13 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Offer from '../offer/offer';
 
-const ListOfOffersNearby = ({offers}) => {
+const ListOfOffersNearby = ({offers, onChnageActiveOfferId = () => {}}) => {
+  const changeActiveOfferIdHandler = React.useCallback((id) => onChnageActiveOfferId(id), []);
   return (
     <section className="near-places places">
       <h2 className="near-places__title">Other places in the neighbourhood</h2>
       <div className="near-places__list places__list">
-        {offers.slice(0, 3).map((item, i) => <Offer key={item.title + i * 2} {...item}/>)}
+        {offers.slice(0, 3).map((item, i) => <Offer key={item.title + i * 2} {...item} onChnageActiveOfferId={changeActiveOfferIdHandler}/>)}
       </div>
     </section>);
 };
@@ -15,6 +16,7 @@ const ListOfOffersNearby = ({offers}) => {
 
 ListOfOffersNearby.propTypes = {
   offers: PropTypes.array.isRequired,
+  onChnageActiveOfferId: PropTypes.func,
 };
 
 export default ListOfOffersNearby;
